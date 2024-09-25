3bbcd3ba808dd4fe7b7d8a8a96a9fc1aba1220c3
Jim Shortz <farx1024@outlook.com>
2024-09-24 21:48:49 -0400

    0.01Hz accuracy maybe?

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 audioclock:	audioclock.c
-	gcc -o audioclock -g audioclock.c
+	gcc -o audioclock -g -Wall audioclock.c
+
+test:	audioclock
+	./audioclock | pacat --rate=48000 --channels=1 --raw
--- audioclock.c
+++ audioclock.c
@@ -16,16 +16,16 @@
 
 int16_t buf[SAMP];		/* Buffer holding 1 second of audio */
 
-/* Generate a 1 second tone of freq and output it to stdout */
-void play(int freq) {
-    int i,j, period, rem, err,p;
+/* Generate a 100 second tone of freq and output it to stdout */
+void play(int64_t freq) {
+    int64_t i,j, period, rem, err,p;
     int cnt=0; 			/* Actual frequency count */
     
-    period = SAMP / 2 /freq;	/* Number of samples per half-cycle */
-    rem = (SAMP / 2) % freq;	/* Number of partial samples lost in the divide */
+    period = SAMP * 50 / freq;	/* Number of samples per half-cycle */
+    rem = (SAMP * 50) % freq;	/* Number of partial samples lost in the divide */
     err = 0;			/* Accumulated error */
     
-    for (i=0 ; i<SAMP; ) {
+    for (i=0;i < SAMP;) {
 	p = period;		/* Period for this iteration */
 	if (err*2 >= freq) {
 	    /* Add an extra sample if we have accumulated enough error */
@@ -34,11 +34,11 @@ void play(int freq) {
 	}
 	cnt++;
 	/* Do the high part */
-	for (j=0;j<p;j++) {
+	for (j=0;j<p && i<SAMP;j++) {
 	    buf[i++] = HI;
 	}
 	/* And the low part */
-	for (j=0;j<p;j++) {
+	for (j=0;j<p && i<SAMP;j++) {
 	    buf[i++] = LO;
 	}
 	
@@ -46,20 +46,23 @@ void play(int freq) {
     }
 
     /* Write diagnostic data to console */
-    fprintf(stderr, "freq=%d, period=%d, rem=%d ", freq, period, rem);
-    fprintf(stderr, "cnt=%d\n", cnt);
+//    fprintf(stderr, "freq=%d, period=%d, rem=%d ", freq, period, rem);
+//    fprintf(stderr, "cnt=%d\n", cnt);
 
     /* Emit sample data to stdout */
     fwrite(buf, sizeof(buf), 1, stdout);
 }
 
 int main(int argc, char**argv) {
-    int h,m;
+    int h,m,s;
 
     /* Simulate an HHMM clock going from 0400 to 2359 */
     for (h=1;h<24;h++) {
 	for (m=0;m<60;m++) {
-	    play(h*100 + m);
+	    for (s=0;s<60;s++) {
+		fprintf(stderr,"%02d:%02d:%02d\n", h, m, s);
+		play((h*100 + m)*100 + s);
+	    }
 	}
     }
 }
